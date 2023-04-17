6ae4f02404f6a9acbbcee204e410d5eed456de68
TenGreen <Grishintut@yandex.ru>
2023-04-17 13:23:05 +0300

    Поправил код. Ввёл isEquals. Исключил возможные повторные изменения элементов внешним циклом при условии, что повторов больше одного.

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         String stringPoem[] = new String[10];
-        stringPoem[0] = "Мой";
-        stringPoem[1] = "дядя";
-        stringPoem[2] = "не в шутку";
-        stringPoem[3] = "самых";
-        stringPoem[4] = "честных";
-        stringPoem[5] = "правил";
-        stringPoem[6] = "когда";
-        stringPoem[7] = "не в шутку";
-        stringPoem[8] = "не в шутку";
-        stringPoem[9] = "Мой";
+        stringPoem[0] = "Мой ";
+        stringPoem[1] = "дядя ";
+        stringPoem[2] = "не в шутку ";
+        stringPoem[3] = "самых ";
+        stringPoem[4] = "честных ";
+        stringPoem[5] = "правил ";
+        stringPoem[6] = "когда ";
+        stringPoem[7] = "не в шутку ";
+        stringPoem[8] = "не в шутку ";
+        stringPoem[9] = "Мой ";
+        boolean isEquals = false;
 
         boolean mask[] = new boolean[stringPoem.length];
-
-        stringPoem.toString();
 
         for (int i = 0; i < stringPoem.length - 1; i++) {
             for (int j = i + 1; j < stringPoem.length; j++) {
@@ -22,9 +21,14 @@ public class Main {
                     continue;
                 }
                 if (stringPoem[i].equals(stringPoem[j])) {
-                    mask[i] = true;
+                    isEquals = true;
+                    //mask[i] = true;
                     mask[j] = true;
                 }
+            }
+            if (isEquals) {
+                stringPoem[i] = null;
+                isEquals = false;
             }
         }
         for (int i = 0; i < stringPoem.length; i++) {
@@ -33,7 +37,7 @@ public class Main {
             }
         }
         for (int i = 0; i < stringPoem.length; i++) {
-            if (!mask[i]) {
+            if (stringPoem[i] != null) {
                 System.out.print(stringPoem[i]);
             }
         }
